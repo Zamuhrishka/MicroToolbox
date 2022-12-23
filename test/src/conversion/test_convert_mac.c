b440f76eccc32ce19165939bf9d26d3028750418
Alexander <roux@yandex.ru>
2022-12-24 00:07:25 +0100

    Add separate file for MAC convert unit tests

--- test/src/conversion/test_convert_mac.c
+++ test/src/conversion/test_convert_mac.c
@@ -1,8 +1,10 @@
 /**
-* \file         template.c
-* \author       Kovalchuk Alexander (roux@yandex.ru)
-* \brief        This file contains the prototypes functions which use for...
-*/
+ * @file    test_convert_mac.c
+ * @author  Aleksander Kovalchuk (roux@yandex.ru)
+ * @brief   This file contains unit tests for convert MAC to string and 
+ *          vice versa functions from @conversion.c file
+ */
+
 //_____ I N C L U D E S _______________________________________________________
 #include "unity.h"
 
@@ -19,27 +21,33 @@
 //_____ V A R I A B L E S _____________________________________________________
 //_____ P R I V A T E  F U N C T I O N S_______________________________________
 //_____ P U B L I C  F U N C T I O N S_________________________________________
-
-
-void test_ConvertStringToMacAddr(void)
+/**
+ * @brief Unit test for convert sting of valid MAC to mac_t type.
+ */
+void test_ConvertStringToMacAddr_Valid(void)
 {
-	bool result = false;
+	mac_t expected = {0xAA, 0x13, 0x01, 0x00, 0x55, 0xFE};	
+	char actual[] = "AA:13:01:00:55:FE";
+	mac_t mac = {};
 
-	mac_t patternMacAddr = {0xAA, 0x13, 0x01, 0x00, 0x55, 0xFE};
-	mac_t mac = {0};
+	bool result = convert_string_to_mac_addr(actual, &mac);
+	TEST_ASSERT_TRUE(result);
+	TEST_ASSERT_EQUAL_MEMORY(expected.value, mac.value, sizeof(expected.value));	
+}
 
-	char patternMacAddrStr1[] = "AA:13:01:00:55:FE\0";
-	char patternMacAddrStr2[] = "AA!13,01600:55:FE\0";
-	char patternMacAddrStr3[] = "PA:13:l1:00:55:FE\0";
-	char patternMacAddrStr4[] = "It`s not a MAC address\0";
-	char patternMacAddrStr5[] = "AA13010055FE\0";
+/**
+ * @brief Unit test for convert sting of invalid MAC to mac_t type.
+ */
+void test_ConvertStringToMacAddr_Invalid(void)
+{
+	mac_t mac = {};
+	char patternMacAddrStr2[] = "AA!13,01600:55:FE";
+	char patternMacAddrStr3[] = "PA:13:l1:00:55:FE";
+	char patternMacAddrStr4[] = "It`s not a MAC address";
+	char patternMacAddrStr5[] = "AA13010055FE";
 	char patternMacAddrStr6[] = "";
 
-	result = convert_string_to_mac_addr(patternMacAddrStr1, &mac);
-	TEST_ASSERT_TRUE(result);
-	TEST_ASSERT_EQUAL_MEMORY(patternMacAddr.value, mac.value, sizeof(patternMacAddr.value));
-
-	result = convert_string_to_mac_addr(patternMacAddrStr2, &mac);
+	bool result = convert_string_to_mac_addr(patternMacAddrStr2, &mac);
 	TEST_ASSERT_FALSE(result);
 
 	result = convert_string_to_mac_addr(patternMacAddrStr3, &mac);
@@ -56,20 +64,18 @@ void test_ConvertStringToMacAddr(void)
 
 	result = convert_string_to_mac_addr(NULL, &mac);
 	TEST_ASSERT_FALSE(result);
-
-	result = convert_string_to_mac_addr(patternMacAddrStr1, NULL);
-	TEST_ASSERT_FALSE(result);
 }
 
+/**
+ * @brief Unit test for convert mac_t type to sting.
+ */
 void test_ConvertMacAddrToString(void)
 {
-	bool result = false;
-
 	mac_t patternMacAddr = {0xAA, 0x13, 0x01, 0x00, 0x55, 0xFE};
-	char patternMacAddrStr[] = "AA:13:01:00:55:FE\0";
-	char mac[] = "00:00:00:00:00:00\0";
+	char patternMacAddrStr[] = "AA:13:01:00:55:FE";
+	char mac[] = "00:00:00:00:00:00";
 
-	result = convert_mac_addr_to_string(&patternMacAddr, mac);
+	bool result = convert_mac_addr_to_string(&patternMacAddr, mac);
 	TEST_ASSERT_TRUE(result);
 	TEST_ASSERT_EQUAL_STRING(mac, patternMacAddrStr);
 }
